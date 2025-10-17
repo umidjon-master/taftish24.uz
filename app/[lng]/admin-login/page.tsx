57f66fbd471fa537447549a9fa8dc569855f44ae
umidjon-master <gafforovumidjon849@gmail.com>
2025-10-18 01:07:24 +0500

    :fix eslint

--- app/[lng]/admin-login/page.tsx
+++ app/[lng]/admin-login/page.tsx
@@ -21,7 +21,7 @@ function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="/tashtif.png"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
